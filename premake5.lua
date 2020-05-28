ec99bfe233f3a6af3bafa24e10a7a39f7f9c5098
mattateusb7 <mattateusb7@gmail.com>
2020-05-28 01:32:28 +0100

    Added Premake Project Setup

---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Comphi"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Comphi"
+    location "Comphi"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"     
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CPHI_WINDOWS_PLATFORM",
+            "CPHI_BUILD_DLL" 
+        }
+
+        postbuildcommands
+        {
+            ("{MKDIR} ../bin/".. outputdir .."/Sandbox"),
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/".. outputdir .."/Sandbox")
+        }
+
+        cleancommands { "" }
+    
+    filter "configurations:Debug"
+        defines "CPHY_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release"
+        defines "CPHY_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "CPHY_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Comphi/vendor/spdlog/include",
+        "Comphi/src"
+    }
+
+    links
+    {
+        "Comphi"
+    }
+
+    filter "system:windows"     
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CPHI_WINDOWS_PLATFORM"
+        }
+
+    filter "configurations:Debug"
+        defines "CPHY_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release"
+        defines "CPHY_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "CPHY_DIST"
+        optimize "On"
